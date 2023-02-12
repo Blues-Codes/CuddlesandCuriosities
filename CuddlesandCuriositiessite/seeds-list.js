3705082b54ded5d06e2a3363cafcea42a3358f48
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-02-11 22:30:32 -0500

    started database-Ami done, clothing started

---
+++ CuddlesandCuriositiessite/seeds-list.js
@@ -0,0 +1,247 @@
+<h4>Database list </h4>
+
+Amigurumi ({
+
+title: "Toothless",
+level: "Intermediate",
+itemsNeeded: [
+    "Black yarn", 
+    "black and green felt",
+    "yarn needle",
+    "stitch markers",
+    "polyfil stuffing",
+    "black yarn",
+]
+hookNeeded: "4.25 mm - US terms",
+yarnType: "4 - Worsted/Aran",
+image: <img src =".//CuddlesandCuriositiessite/images/Amigurumi/Toothless-img.png" alt ="toothless dragon"/>
+Stitches: [
+    "Ch - Chain",
+    "SC - single crochet",
+    "INC - single crochet increase",
+    "DEC - single crochet decrease", 
+    "HDC - Half-double Crochet",
+    "BLO - Back-loop only",
+    "MR - Magic ring", 
+    "SL ST - Slip Stitch",
+    "FO - Fasten off",
+],
+creator: "Nichole's Nerdy Knots",
+creatorLink: <a href= "www.facebook.com/nicholesnerdyknots"> nicholesnerdyknots</a>;
+}
+{
+title: "Squirt",
+level: "Intermediate",
+itemsNeeded: [
+    "Cherry Red yarn", 
+    "warm brown yarn",
+    "yarn needle",
+    "stitch markers",
+    "2 skeing of soft green yarn",
+    "white yarn",
+    "black yarn",
+    "Polyfil stuffing",
+]
+hookNeeded: "4.25 mm - US terms",
+yarnType: "4 - Worsted/Aran",
+image: <img src ="../CuddlesandCuriositiessite/images/Amigurumi/Squirt.png" alt ="Squirt sea turtle"/>
+Stitches: [
+    "Ch - Chain",
+    "SC - single crochet",
+    "DC - double crochet increase",
+    "TR - Triple crochet decrease", 
+    "HDC - Half-double Crochet",
+    "DC2TOG - DC decrease",
+    "DC3TOG - DC decrease with 3 stitches", 
+    "SL ST - Slip Stitch",
+    "FO - Fasten off",
+],
+creator: "Cuddles and Curiosities",
+creatorLink: <a href= ""> </a>;
+}
+{
+title: "Baby Sully",
+level: "Hopeful Beginner",
+itemsNeeded: [
+    "Color A: turquoise (60 yds)",
+    "Color B: dark blue (3 yds)",
+    "Color C: yellow (5 yds)",
+    "Color D: lavender (2 yds)",
+    "2 8mm saftey eyes",
+    "Polyfil stuffing",
+    "yarn needle",
+]
+hookNeeded: "3.75 mm - US terms",
+yarnType: "4 - Worsted/Aran",
+image: <img src ="../images/Amigurumi/Baby-Sully.png" alt ="Baby Sully from Monsters INC"/>
+Stitches: [
+    "MR - Magic ring", 
+    "Ch - Chain",
+    "SC - single crochet",
+    "DEC - single crochet decrease",
+    "BLO - Back-loop only",
+    "FLO - Front-loop only",
+    "INC - single crochet increase",
+    "SL ST - slip stitch",
+    
+    
+],
+creator: "A Morning Cup of Jo Creations",
+creatorLink: <a href= "https://www.facebook.com/amorningcupofjocreations">A Morning Cup of Jo Creations </a>;
+}
+{
+title: "Baby Mike",
+level: "Hopeful Beginner",
+itemsNeeded: [
+    "Color A: lime green (30 yds)",
+    "Color B: yellow (1 yd)",
+    "PolyFil Stuffing",
+    "1 12 MM safety eye",
+    "White and Blue felt",
+    "Fabric glue",
+    "Yarn Needle",
+    
+]
+hookNeeded: "3.75 mm - US terms",
+yarnType: "4 - Worsted/Aran",
+image: <img src ="../images/Amigurumi/Baby-Mike.png" alt ="baby Mike from Monsters Inc"/>
+Stitches: [
+    "Ch - Chain",
+    "SC - single crochet",
+    "DEC - single crochet decrease",
+    "SL ST - Slip Stitch", 
+    "INC - single crochet increase",
+    
+],
+creator: "A Morning Cup of Jo Creations",
+creatorLink: <a href= "https://www.facebook.com/amorningcupofjocreations">A Morning Cup of Jo Creations </a>;
+
+},
+{
+title: "Baphoment - Kawaii",
+level: "Hopeful Beginner",
+itemsNeeded: [
+    "4 colors of your choice",
+    "Black Embroidery Floss",
+    "PolyFil Stuffing",
+    "20MM Safety eyes",
+    "Scissors",
+    "Fabric glue",
+    "Yarn Needle",
+    "5mm/10mm jump-rings for earrings (optional)",
+    
+]
+hookNeeded: "3 mm and 3.5mm - US terms",
+yarnType: "4 - Worsted/Aran",
+image: <img src ="CuddlesandCuriositiessite/images/Amigurumi/Baphoment-Kawaii.png" alt ="Pink and red Baphomet doll "/>
+Stitches: [
+    "CH - Chain",
+    "SC - single crochet",
+    "DEC - single crochet decrease",
+    "SL ST - Slip Stitch", 
+    "INC - single crochet increase",
+    "MR - Magic Ring",
+    "INC3 - 3 single crochet",
+    "INC5 - 5 single crochet",
+    "3 SCTOG - 3 single crochet decrease",
+    "FO - fasten off"
+    
+],
+creator: "GATO FU",
+creatorLink: <a href= "http://www.etsy.com/shop/gatofu">GATO FU</a>;
+}
+{
+    title: "Stitch",
+    level: "Intermediate",
+    itemsNeeded: [
+        "Blue/Grey Yarn",
+        "Light Blue Yarn",
+        "PolyFil Stuffing",
+        "Pink, black and white roving wool",
+        ]
+    hookNeeded: "4 mm - US terms",
+    yarnType: "4 - Worsted/Aran",
+    image: <img src ="CuddlesandCuriositiessite/images/Amigurumi/Stitch.png" alt ="stitch doll "/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "SL ST - Slip Stitch", 
+        "INC - single crochet increase",
+        "MR - Magic Ring",
+        "HDC - half double crochet",
+        "BLO - back-loop only",
+        "TR - Triple crochet",
+        "FO - fasten off"
+        
+    ],
+    creator: "Rolly Crochet",
+    creatorLink: <a href= "blog.pianetadonna.it/rollycrochet/">Rolly Crochet</a>;
+},
+{
+    title: "3D Skull ",
+    level: "Advanced",
+    itemsNeeded: [
+        "White yarn",
+        "Black embroidery floss",
+        "floral wire",
+        "stitch markers",
+        "Firm polyfil stuffing",
+        "tapestry needle",
+        "scissors"
+        ]
+    hookNeeded: "3 mm - US terms",
+    yarnType: "3 - Light worsted/DK",
+    image: <img src ="CuddlesandCuriositiessite/images/Amigurumi/AMI-Skull.png" alt ="skull"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "SL ST - Slip Stitch", 
+        "INC - single crochet increase",
+        "MR - Magic Ring",
+        "HDC - half double crochet",
+        "RSM - running stitch marker",
+        "DC - double crochet",
+        "FO - fasten off"
+        
+    ],
+    creator: "Million Bells",
+    creatorLink: <a href= "http://www.etsy.com/shop/millionbells">Million Bells</a>;
+})
+
+Clothing {
+
+    title: "Starry Night Sweater ",
+    level: "Advanced",
+    itemsNeeded: [
+        "1,200 yards of Main Color Yarn ",
+        "50 Yards of Dark Dark Blue yarn",
+        "50 Yards of Dark Blue yarn",
+        "50 Yards of Blue yarn",
+        "50 Yards of Light Blue yarn",
+        "10 Yards of yellow yarn",
+        "20 yards of black yarn",
+        "Stitch markers",
+        "tapestry needle"
+        ]
+    hookNeeded: "5.5 mm - US terms",
+    yarnType: "4 - Worsted/Aran",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/Starry-Night-sweater.png" alt ="starry night sweater"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "SL ST - Slip Stitch", 
+        "INC - single crochet increase",
+        "MS - mattress stitch",
+        "WS - whip stitch",
+        "TC - tapestry crochet",
+        "IC - intarsia crochet",
+        "DC - double crochet",
+        "BLO - back-loop only"
+        
+    ],
+    creator: "Kayla Genato",
+    creatorLink: <a href= "http://www.instagram.com/craftsbygelato">Kayla Genato</a>;
+}
--- CuddlesandCuriositiessite/seeds.js
+++
@@ -1,1 +0,0 @@
-<h3>Database seed</h3>
