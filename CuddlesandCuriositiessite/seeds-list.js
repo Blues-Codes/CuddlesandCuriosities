52a560fa89f8466306bba688783c4e792bcfdd2b
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-02-12 13:54:20 -0500

    Database list done, create form done

--- CuddlesandCuriositiessite/seeds-list.js
+++ CuddlesandCuriositiessite/seeds-list.js
@@ -208,10 +208,57 @@ creatorLink: <a href= "http://www.etsy.com/shop/gatofu">GATO FU</a>;
     ],
     creator: "Million Bells",
     creatorLink: <a href= "http://www.etsy.com/shop/millionbells">Million Bells</a>;
+}
+{
+    title: "Willendorf Venus ",
+    level: "Advanced",
+    itemsNeeded: [
+        "light brown or beige yarn",
+         "stitch markers",
+        "Firm polyfil stuffing",
+        "tapestry needle",
+        "scissors"
+        ]
+    hookNeeded: "2.25mm - US terms",
+    yarnType: "1 - super fine/fingering",
+    image: <img src ="CuddlesandCuriositiessite/images/Amigurumi/venus-willendorf.png" alt ="Venus Willendorf"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "SL ST - Slip Stitch", 
+        "INC - single crochet increase",
+        "MR - Magic Ring",
+        "RD - round"
+        
+    ],
+    creator: "Trishagurumi",
+    creatorLink: <a href= "">Trishagurumi</a>;
 })
 
-Clothing {
+Clothing ({
 
+    title: "Broomstick Cowl ",
+    level: "Advanced",
+    itemsNeeded: [
+        "160 yards of any color yarn",
+        "Stitch markers",
+        "tapestry needle",
+        "1 medium or large button",
+        "25 mm knitting needle or pin"
+         ]
+    hookNeeded: "3.5 mm, 4 mm - US terms",
+    yarnType: "3 - Light Worsted/DK ",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/Broomstick-cowl.png" alt ="Broomstick cowl"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+                
+    ],
+    creator: "Mon Petit Violon Designs",
+    creatorLink: <a href= "http://www.monpetitviolon.com/">Mon Petit Violon Designs</a>;
+}
+{
     title: "Starry Night Sweater ",
     level: "Advanced",
     itemsNeeded: [
@@ -245,3 +292,151 @@ Clothing {
     creator: "Kayla Genato",
     creatorLink: <a href= "http://www.instagram.com/craftsbygelato">Kayla Genato</a>;
 }
+{
+    title: "Checkered Cowl ",
+    level: "Beginner",
+    itemsNeeded: [
+        "3 skeins of yarn",
+        "Stitch markers",
+        "yarn needle"
+        ]
+    hookNeeded: "6.5 mm - US terms",
+    yarnType: "4 - Worsted/Aran",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/Checkered-cowl.png" alt ="checkered cowl"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "DC - double crochet",
+        "BPDC - back post double crochet",
+        "FPDC - front post double crochet"
+        
+    ],
+    creator: "Yarnspirations - Designed by Heather Lodinsky",
+    creatorLink: <a href= "http://www.yarnspirations.com">Yarnspirations</a>;
+}
+{
+    title: "Single Strap Sweater Dress ",
+    level: "Intermediate",
+    itemsNeeded: [
+        "1400 yards of yarn",
+        "Stitch markers",
+        "yarn needle",
+        "tape measure",
+        ]
+    hookNeeded: "5 mm, 7 mm - US terms",
+    yarnType: "4 - Worsted/Aran",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/single-strap-sweater-dress.png" alt ="Single strap sweater dress"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "DC - double crochet",
+        "BLO - back-loop only",
+        "SL-ST - slip stitch"
+        
+    ],
+    creator: "TCDDIY",
+    creatorLink: <a href= "https://youtu.be/ScUJ3Aay_L8">TCDDIY</a>;
+}
+{
+    title: "Curvy Pull Over ",
+    level: "hopeful beginner",
+    itemsNeeded: [
+        "Yarn amount varies",
+        "Stitch markers",
+        "yarn needle",
+        "tape measure",
+        ]
+    hookNeeded: "9 mm - US terms",
+    yarnType: "6 - Super Bulky",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/Curvy-pull-over.png" alt ="Curvy pullover"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",
+        "DC - double crochet",
+        "BLO - back-loop only",
+        "SL-ST - slip stitch",
+        "HDC - half double crochet"
+        
+    ],
+    creator: "Yarnspirations",
+    creatorLink: <a href= "https://www.yarnspirations.com">Yarnspirations</a>;
+}
+{
+    title: "Hazy Unicorn Shawl ",
+    level: "hopeful beginner",
+    itemsNeeded: [
+        "6 skeins of yarn",
+        "Stitch markers",
+        "yarn needle",
+        "tape measure",
+        ]
+    hookNeeded: "7 mm - US terms",
+    yarnType: "6 - Super Bulky",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/Curvy-pull-over.png" alt ="Curvy pullover"/>
+    Stitches: [
+        "CH - Chain",
+        "SC - single crochet",
+        "DEC - single crochet decrease",        
+    ],
+    creator: "Hobbi",
+    creatorLink: <a href= "http://shop.hobbii.com/hazy-unicorn-shawl">Hobbi</a>;
+}
+{
+    title: "Madison Shoulder Wrap ",
+    level: "intermediate",
+    itemsNeeded: [
+        "1300-1500 yards of yarn A",
+        "1200 - 1400 yards of yarn B",
+        "Stitch markers",
+        "yarn needle",
+        "tape measure",
+        ]
+    hookNeeded: "3mm, 4mm, 5mm - US terms",
+    yarnType: "3 - Light Worsted/DK for yarn A, 4 - Worsted/Aran",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/Madison-shoulder-wrap.png" alt ="Madison shoulder wrap"/>
+    Stitches: [
+        "CH - Chain",
+        "CH SP - chain space",
+        "SC - single crochet",
+        "DC - double crochet",
+        "TR - triple crochet",
+        "HDC - half double crochet",
+        "HDC3L - half double croch in 3rd loop",
+        "TR2TOG - triple crochet decrease with 2 chair spaces",
+        "FPDC - front post double crochet",
+        "BBL - bobble stitch",
+        "FHDC - foundation half double crochet"
+                
+    ],
+    creator: "Christel Riley Watts",
+    creatorLink: <a href= "">Christel Riley Watts</a>;
+}
+{
+    title: "Skull Cocoon Sweater ",
+    level: "intermediate",
+    itemsNeeded: [
+        "4 skeins of yarn",
+        "Stitch markers",
+        "yarn needle",
+        "tape measure",
+        ]
+    hookNeeded: "6mm - US terms",
+    yarnType: "5 - Chunky",
+    image: <img src ="CuddlesandCuriositiessite/images/Clothing/skull-cocoon-sweater.png" alt ="skull cocoon sweater"/>
+    Stitches: [
+        "CH - Chain",
+        "SL ST - slip stitch",
+        "CH SP - chain space",
+        "SC - single crochet",
+        "DC - double crochet",
+        "TR - triple crochet",
+        "HDC - half double crochet",
+        "SP - space"
+                
+    ],
+    creator: "Caryns Creations",
+    creatorLink: <a href= "">Caryns Creations</a>;
+})
